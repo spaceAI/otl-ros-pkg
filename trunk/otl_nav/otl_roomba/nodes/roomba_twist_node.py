00b41352f42731422f0a9c25f595040911d8a55d
t.ogura <t.ogura@c2379714-004c-11df-95d2-51bf8322dccd>
2010-05-21 14:06:45 +0000

    add nodes

---
+++ trunk/otl_nav/otl_roomba/nodes/roomba_twist_node.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import roslib
+roslib.load_manifest('otl_roomba')
+
+import rospy
+from otl_roomba.roombaif import *
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Bool
+from roomba_base_node import RoombaBaseNode
+from roomba_odometry_publisher import RoombaOdometryPublisher
+
+class RoombaTwistNode(RoombaBaseNode):
+    """
+    roomba interface for ROS (twist vel interface)
+    """
+    def _send_vel_cb(self, twist):
+        self._rmb.set_twist_vel(twist.linear.x, twist.angular.z)
+        self._odom.vel = [0.001 * twist.linear.x, 0, twist.angular.z]
+#        self._odom.proc()
+        if ( self._rmb.get_voltage() < 13 * 1000):
+            self._rmb.play_song(0)
+
+    def main(self):
+        rospy.Subscriber('roomba/command',
+                         Twist, self._send_vel_cb, queue_size=1)
+        rospy.Subscriber('roomba/clean',
+                         Bool, self._send_clean_cb, queue_size=1)
+
+        rate = rospy.Rate(20)
+        while not rospy.is_shutdown():
+            self._odom.proc()
+            rate.sleep()
+        self.close()
+
+    def set_odometry(self, rop):
+        self._odom = rop
+
+if __name__ == '__main__':
+    rmb = RoombaIf()
+    rn = RoombaTwistNode(rmb)
+    rop = RoombaOdometryPublisher(rmb)
+    rn.set_odometry(rop)
+    rn.main()
