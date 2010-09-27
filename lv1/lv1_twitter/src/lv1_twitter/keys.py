f5d17df97b9f8e8a68e01f990edf92ac596dd6b5
t.ogura <t.ogura@c2379714-004c-11df-95d2-51bf8322dccd>
2010-09-27 07:12:49 +0000

    delete1

--- lv1/lv1_twitter/src/lv1_twitter/keys.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'feliLoH9Zlp6hfxO8jhEA'
-consumer_secret = 'OtVjrXVezSMxVJrf99HlfTr2fsHQxcEJaAPdxfStkA'
-access_token = '4416471-cKU7HJPDnQt5yAZ98zbhSertqhSyLFSnntX1EQhbaw'
-access_token_secret = 'S68gl5qK11gIYAhRSnkDLJQQUy4I2Goei3tUVg5F0E'
